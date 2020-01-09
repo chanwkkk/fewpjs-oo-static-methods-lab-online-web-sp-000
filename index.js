48980fe310adb3703cd22d1c81f73911bba4963c
Yingqi Chen <chanwk18@hotmail.com>
2020-01-09 01:02:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1)
+  }
+
+  static sanitize(string){
+    string.replace(/[^a-zA-Z0-9-' ]/g, '')
+  }
+
+  static titleize(string){
+    let stringArray = string.split(' ')
+
+  }
+
+
 }
